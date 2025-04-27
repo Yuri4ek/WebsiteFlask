dba52847050926e8819fac8075240f6e6cc843d9
Yuri4ek <dinourski@gmail.com>
2025-04-27 23:05:40 +0300

    тестовый полурабочий вариант

--- data/filling_db/processors.py
+++ data/filling_db/processors.py
@@ -1,26 +1,52 @@
 import requests
 from bs4 import BeautifulSoup
+from pprint import pprint
 
 
 def parse_cpu_info(processor, url):
     # Send HTTP request and get page content
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'}
     response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Dictionary to store results
-    specs = {}
+    # берем html
+    soup = BeautifulSoup(response.text, 'html.parser')
 
     # Find ranking
     a = soup.find_all('em')
-    rating = int(a[1].get_text())
-    cores = int(a[21].get_text().split()[0])
-    threads = int(a[23].get_text())
-    clock_speed = float(a[25].get_text().split()[0])
-    socket = a[47].get_text()
-    tdp = int(a[49].get_text().split()[0])
-    memory_type = a[59].get_text()
-    pcie_type = float(a[63].get_text())
+    print(a)
+    try:
+        rating = int(a[0].get_text())
+    except:
+        rating = int(a[1].get_text())
+    try:
+        cores = int(a[20].get_text().split()[0])
+    except:
+        cores = int(a[21].get_text().split()[0])
+    try:
+        threads = int(a[22].get_text())
+    except:
+        threads = int(a[23].get_text())
+    try:
+        clock_speed = float(a[24].get_text().split()[0])
+    except:
+        clock_speed = float(a[25].get_text().split()[0])
+    try:
+        socket = a[44].get_text()
+    except:
+        socket = a[47].get_text()
+    try:
+        tdp = int(a[46].get_text().split()[0])
+    except:
+        tdp = int(a[49].get_text().split()[0])
+    try:
+        memory_type = a[56].get_text()
+    except:
+        memory_type = a[59].get_text()
+    try:
+        pcie_type = float(a[60].get_text())
+    except:
+        pcie_type = float(a[63].get_text())
+
     print(processor, rating, cores, threads, clock_speed, socket, tdp, memory_type, pcie_type)
 
 
@@ -47,9 +73,12 @@ for i in range(1, 19):
                     processors.append(data[1])
             except:
                 pass
+        break
     else:
         print(f'Ошибка при загрузке страницы: {response.status_code}')
 
 for processor in processors:
+    print(processor)
+    print(f"https://technical.city/en/cpu/{processor.replace(' ', '-')}")
     url = f"https://technical.city/en/cpu/{processor.replace(' ', '-')}"
     parse_cpu_info(processor, url)

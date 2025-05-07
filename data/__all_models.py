72b2f3103c63be0cb4fd7da72d7fc9a8fbe13e82
Yuri4ek <dinourski@gmail.com>
2025-05-07 22:18:06 +0300

    добавил документацию

--- data/__all_models.py
+++ data/__all_models.py
@@ -1,14 +1,14 @@
 from . import processors
 from . import sockets
 from . import memory_types
-from . import cooling_systems
-from . import motherboards
 from . import ram_modules
 from . import videocards
 from . import HDDs
 from . import SSDs
 from . import power_supplies
 from . import computer_cases
+from . import cooling_systems
+from . import motherboards
 
 from . import users
 from . import configurations
